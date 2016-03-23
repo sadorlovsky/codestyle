c70851f2db2c09a93fe86c5f005c34deaa753531
Zach Eagle <mazaxakep@gmail.com>
2016-03-23 20:25:07 +0300

    parser test

--- test/rules/parser.js
+++ test/rules/parser.js
@@ -1,7 +1,12 @@
 import test from 'ava'
 import cli from './_helper'
 
-test('async-await works', t => {
+test('async/await', t => {
   const code = 'async function load () {\n  await fetch(\'/feed\')\n}\n'
+  t.is(cli.executeOnText(code).errorCount, 0)
+})
+
+test('decorator', t => {
+  const code = '@bind\nclass Doge {\n\n}\n'
   t.is(cli.executeOnText(code).errorCount, 0)
 })
