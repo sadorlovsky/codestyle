d72bacfc8e2ef7b656b03969fd6345ff2a4b716b
Zach Eagle <mazaxakep@gmail.com>
2016-03-24 18:41:20 +0300

    change tests title

--- test/rules/arrow-parens.js
+++ test/rules/arrow-parens.js
@@ -1,22 +1,22 @@
 import test from 'ava'
 import { cli } from './_helper'
 
-test('no parens is good', t => {
+test('no arrow-parens is good', t => {
   const code = 'export const double = x => x * 2\n'
   t.is(cli.executeOnText(code).errorCount, 0)
 })
 
-test('parens are bad', t => {
+test('arrow-parens are bad', t => {
   const code = 'export const double = (x) => x * 2\n'
   t.is(cli.executeOnText(code).errorCount, 1)
 })
 
-test('arrow parens', t => {
+test('arrow-parens are good if more than one arg', t => {
   const code = 'export const multiply = (x, y) => x * y\n'
   t.is(cli.executeOnText(code).errorCount, 0)
 })
 
-test('arrow parens', t => {
+test('no arrow-parens is bad if more than one arg', t => {
   const code = 'export const multiply = x, y => x * y\n'
   t.is(cli.executeOnText(code).errorCount, 1)
 })
