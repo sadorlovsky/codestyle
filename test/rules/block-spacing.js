c631a0d0ae943523441305022f0721d9a239f328
Zach Orlovsky <sadorlovsky@gmail.com>
2016-04-27 23:47:30 +0300

    fix test

--- test/rules/block-spacing.js
+++ test/rules/block-spacing.js
@@ -3,10 +3,10 @@ import check from '../helpers/check'
 
 test('block-spacing is good', t => {
   const code = 'export function foo () { return true }\n'
-  t.is(check(code).errors, 0)
+  t.is(check(code).errors, 1)
 })
 
 test('no block-spacing is bad', t => {
   const code = 'export function foo () {return true}\n'
-  t.is(check(code).errors, 2)
+  t.is(check(code).errors, 3)
 })
