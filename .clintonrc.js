3ef5aa0ac58eaf59d965575f6fd8171e389f2e8a
Zach Orlovsky <sadorlovsky@gmail.com>
2016-06-11 17:53:09 +0300

    setup clinton

---
+++ .clintonrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  rules: {
+    'ava': 'error',
+    'editorconfig': 'error',
+    'license': ['error', 'MIT'],
+    'no-callback': 'error',
+    'pkg-files': 'error',
+    'pkg-main': 'error',
+    'pkg-schema': 'error',
+    'readme': 'error',
+    'test-script': 'error',
+    'use-travis': 'error',
+    'valid-version': 'error'
+  }
+}
