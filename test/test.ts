823626060f852055cee941f2d3ca7452236b1cf0
Zach Orlovsky <sadorlovsky@gmail.com>
2020-02-18 01:03:39 +0300

    write test in typescript

--- test/test.ts
+++ test/test.ts
@@ -1,9 +1,9 @@
-const test = require('ava')
-const { CLIEngine } = require('eslint')
-const isPlainObj = require('is-plain-obj')
-const config = require('../index')
-const configReact = require('../react')
-const configAva = require('../ava')
+import test from 'ava'
+import { CLIEngine } from 'eslint'
+import isPlainObj from 'is-plain-obj'
+import config from '../index'
+import configReact from '../react'
+import configAva from '../ava'
 
 test('config is a plain object', t => {
   t.true(isPlainObj(config))
@@ -25,7 +25,7 @@ test('lints', t => {
   const report = cli.executeOnFiles([
     '../codestyle/test/fixtures'
   ])
-  const formatter = CLIEngine.getFormatter()
+  const formatter = cli.getFormatter()
   console.log(formatter(report.results))
   t.is(report.errorCount, 0)
 })
